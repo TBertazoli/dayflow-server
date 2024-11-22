aa99ea2912a768e73fc2e285bdb6498ade2f5fe4
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-11-22 16:23:42 -0600

    configured module for mongoDB using mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri:
+          config.get('DATABASE_URL') || 'mongodb://localhost:27017/defaultdb',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
